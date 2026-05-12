0b772d8b0b54b1ce0bac7fadfed631faaffdc8d0
yuri2612w <wesley.yuri.santos@escola.pr.gov.br>
2026-05-12 07:57:07 -0300

    .

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 const botoes= document.querySelectorAll(".botão")
 console.log(botoes)
+const textos = document.querySelectorAll(".aba-conteudo")
+console.log(textos)
+
+for (let i = 0; i < botoes .length; i++){                                    
+    botoes [i].onclick  = function (){
+        for(let j=0; j<botoes.length;i++){
+            botoes[j].classList.remove("ativo")
+            textos[j].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativoi")
+        textos[i].classList.add("ativo")
+    }  
+}
