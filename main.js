d24dfb1115eabf8bafbb7e704c88e7244360dad4
yuri2612w <wesley.yuri.santos@escola.pr.gov.br>
2026-05-13 12:05:24 -0300

    .

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ for (let i = 0; i < botoes.length; i++){
         textos[i].classList.add("ativo")
     }  
 }
+
+const contadores= document.querySelectorAll(".contador")
+const tempoObjetivo1 = new Date("2026-10-05T00:00:00")
+const tempoObjetivo2 = new Date("2026-10-07T00:00:00")
+const tempoObjetivo3 = new Date("2026-10-09T00:00:00")
+const tempoObjetivo4 = new Date("2026-10-10T00:00:00")
+
+const tempos=[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempofinal = tempoObjetivo- tempoAtual
+    console.log(tempofinal) 
+}
+calculaTempo(tempoObjetivo1)
