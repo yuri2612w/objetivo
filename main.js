9dc7fca11d79a3ed2ba94199e87fed2f17fee087
yuri2612w <wesley.yuri.santos@escola.pr.gov.br>
2026-05-20 12:06:43 -0300

    ,

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function calculaTempo(tempoObjetivo){
 }
 function atualizaCronometro(){
     for(let i=0; i<contadores.length;i++){
-        document.getElementById("dias" +i).textContent = calculaTempo(tempos[i])[0]
+        document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0]
     }
 }
 calculaTempo(tempoObjetivo1)
